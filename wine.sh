8ea0bb90c519137cd4db5e0e8a065af18519546c
EkramulLab <144709600+EkramulLab@users.noreply.github.com>
2023-09-23 22:22:57 +0600

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -23,5 +23,6 @@ sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=eaemon -e USER=emon --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:wine
 clear
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo  emon : eaemon
 sleep 7200000
